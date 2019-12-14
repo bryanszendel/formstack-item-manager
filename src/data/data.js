e8864deffa881d04703957e3d42fcfb3a0d6ab37
Bryan Szendel <bryan.szendel@gmail.com>
2019-12-14 14:22:13 -0700

    added placeholder data

--- src/data/data.js
+++ src/data/data.js
@@ -3,22 +3,42 @@ const formatted = `${today.getMonth() + 1}-${today.getDate()}-${today.getFullYea
 const data = [
   {
     id: 1, 
-    name: 'Item1',
+    name: 'Setup React app using create-react-app',
     selected: false,
     created_at: formatted,
   },
   {
     id: 2, 
-    name: 'Item2',
+    name: 'Install Redux using react-redux',
     selected: false,
     created_at: formatted
   },
   {
     id: 3, 
-    name: 'Item3',
+    name: 'Setup reducers and actions',
     selected: false,
     created_at: formatted
   },
+  {
+    id: 4, 
+    name: 'Connect actions to container components',
+    selected: false,
+    created_at: formatted
+  },
+  {
+    id: 5, 
+    name: 'Pass action props to presentational components',
+    selected: false,
+    created_at: formatted
+  },
+  {
+    id: 6, 
+    name: 'Add some styles using SemanticUI React',
+    selected: false,
+    created_at: formatted
+  },
+
+  
 ]
 
 export default data;
